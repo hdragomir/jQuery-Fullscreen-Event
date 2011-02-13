874fceee1fcab127d0b169d52e0540b054d538fd
Horia Dragomir <horia@hdragomir.com>
2011-02-13 19:43:42 +0200

    optimized edge case handling

--- jquery.onfullscreen-1.0.0.js
+++ jquery.onfullscreen-1.0.0.js
@@ -5,40 +5,34 @@
 
 (function($){
     
-    $.onfullscreen = function(){return $.onfullscreen.prototype.was_fullscreen}
+    $.onfullscreen = function(){return $.onfullscreen.was_fullscreen}
     
     
-    $.onfullscreen.prototype.is_fullscreen = function(){
+    $.onfullscreen.is_fullscreen = function(){
         return window.innerHeight == screen.height && window.innerWidth == screen.width;
-    }
+    };
     
-    $.browser.mozilla && ($.onfullscreen.prototype.is_fullscreen = function(){
+    ( $.browser.mozilla || ( $.browser.msie && ('9' == $.browser.version.toString()[0]) )) && ($.onfullscreen.is_fullscreen = function(){
         var wih = window.innerHeight, sh = screen.height
-        return (wih == sh || wih + 5 == sh ) && window.innerWidth == screen.width;
+        return ( wih >= (sh - 5) ) && window.innerWidth == screen.width;
     });
     
-    
-    $.browser.msie && ('9' == $.browser.version.toString()[0]) && ($.onfullscreen.prototype.is_fullscreen = function(){
-        var wih = window.innerHeight, sh = screen.height
-        return (wih == sh || wih + 1 == sh || wih + 2 == sh) && window.innerWidth == screen.width;
-    });
-    
-    
-    $.onfullscreen.prototype.was_fullscreen = $.onfullscreen.prototype.did_resize = false;
+        
+    $.onfullscreen.was_fullscreen = $.onfullscreen.did_resize = false;
     
     
     $.onfullscreen.prototype.check_resize_interval = setInterval(function(){
-        if($.onfullscreen.prototype.did_resize){
-            $.onfullscreen.prototype.did_resize = false;
-            var check_against = $.onfullscreen.prototype.is_fullscreen();
-            if($.onfullscreen.prototype.was_fullscreen != check_against)
-                $(window).trigger('fullscreen', $.onfullscreen.prototype.was_fullscreen = check_against);
+        if($.onfullscreen.did_resize){
+            $.onfullscreen.did_resize = false;
+            var check_against = $.onfullscreen.is_fullscreen();
+            if($.onfullscreen.was_fullscreen != check_against)
+                $(window).trigger('fullscreen', $.onfullscreen.was_fullscreen = check_against);
         }
     }, 250);
     
     
     $(window).bind('resize', function(){
-        $.onfullscreen.prototype.did_resize = true;
+        $.onfullscreen.did_resize = true;
     }).resize();
 
 })(jQuery);
